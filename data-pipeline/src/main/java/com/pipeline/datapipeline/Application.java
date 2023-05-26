b3998d8f0a5026a6a643949935c84c162471e226
Abhishek <abhirevadekar12@gmail.com>
2023-05-26 13:44:20 -0400

    Updated log4j2, improved MVC structure.

--- data-pipeline/src/main/java/com/pipeline/datapipeline/Application.java
+++ data-pipeline/src/main/java/com/pipeline/datapipeline/Application.java
@@ -1,9 +1,13 @@
 package com.pipeline.datapipeline;
 
-import com.pipeline.datapipeline.controllers.StreamController;
+import com.pipeline.datapipeline.controllers.DataReceiverController;
+import com.pipeline.datapipeline.controllers.DataStreamController;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import java.util.logging.LogManager;
 
 @SpringBootApplication
 public class Application {
@@ -11,8 +15,20 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Application.class, args);
 
-		StreamController t = context.getBean(StreamController.class);
-		t.startStreamController();
+		if(args.length>0) {
+			switch(args[0]) {
+				case "stream":
+					DataStreamController dataStreamController = context.getBean(DataStreamController.class);
+					dataStreamController.startStreamController();
+					break;
+				case "fetch":
+					DataReceiverController dataReceiverController = context.getBean(DataReceiverController.class);
+					dataReceiverController.startStreamController();
+					break;
+				default:
+					System.out.println("Wrong Input");
+			}
+		}
 	}
 
 }
--- data-pipeline/src/main/java/com/pipeline/datapipeline/controllers/DataStreamController.java
+++ data-pipeline/src/main/java/com/pipeline/datapipeline/controllers/DataStreamController.java
@@ -1,34 +1,28 @@
 package com.pipeline.datapipeline.controllers;
 
-import com.pipeline.datapipeline.services.DataReceiver;
-import com.pipeline.datapipeline.services.DataStreamer;
+import com.pipeline.datapipeline.services.DataReceiverService;
+import com.pipeline.datapipeline.services.DataStreamerService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class StreamController {
-
-    private DataStreamer dataStream;
-    private DataReceiver dataReceiver;
+public class DataStreamController {
+    private DataStreamerService dataStreamService;
 
     @Autowired
-    public StreamController(DataStreamer dataStream, DataReceiver dataReceiver) {
-        this.dataStream = dataStream;
-        this.dataReceiver = dataReceiver;
+    public DataStreamController(DataStreamerService dataStreamService) {
+        this.dataStreamService = dataStreamService;
     }
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public void startStreamController() {
 
-
-        Thread receiverThread = new Thread(() -> {
-            dataReceiver.start();
-        });
-        receiverThread.start();
-
         // Start the data processing pipeline
         Thread streamThread = new Thread(() -> {
-            dataStream.start();
+            dataStreamService.start();
         });
         streamThread.start();
 
@@ -37,12 +31,10 @@ public class StreamController {
         // Register shutdown hook to gracefully stop the application
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             // Stop the data processing pipeline
-            dataStream.stop();
-            dataReceiver.stop();
+            dataStreamService.stop();
 
             try {
                 streamThread.join();
-                receiverThread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
